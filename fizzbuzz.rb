e22755b87101cc3d5db15d62378de091bf54b1a3
Phoebe Manning <phoebe.manning8@gmail.com>
2017-09-22 21:11:50 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(num)
   if num % 3 == 0
     "Fizz"
-  elseif num % 5 == 0
+  elsif num % 5 == 0
     "Buzz"
-  elseif ((num % 3 == 0) && (num % 5 == 0))
+  elsif ((num % 3 == 0) && (num % 5 == 0))
     "FizzBuzz"
   else
     ""
